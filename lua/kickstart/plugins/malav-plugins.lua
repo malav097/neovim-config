d323d2afbcf9e8b9db7fe5806a3aac80f66fd866
Simon <simongmalav@gmail.com>
2025-08-20 23:46:27 -0400

    remove markdown rendering shit

--- lua/kickstart/plugins/malav-plugins.lua
+++ lua/kickstart/plugins/malav-plugins.lua
@@ -56,14 +56,15 @@ return {
           },
         },
       },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
+--      {
+--        -- Make sure to set this up properly if you have lazy=true
+--        "MeanderingProgrammer/render-markdown.nvim",
+--        opts = {
+--          enbled = false,
+--          file_types = { "markdown", "Avante" },
+--        },
+--        ft = { "markdown", "Avante" },
+--      },
     },
   },
 
