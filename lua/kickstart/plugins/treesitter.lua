275d1005cb65bdc0d91e158152f33961de705746
Simon <simongmalav@gmail.com>
2026-05-05 18:20:52 -0300

    working stuff

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    commit = 'cf12346a3414fa1b06af75c79faebe7f76df080a',
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
